9c2ed2614e2c52d65387b52d0ce1d43bebf4789b
Itamar Mizrahi <Itamarm@wotch.health>
2022-07-19 21:47:34 +0300

    clean code

--- imagesat-technical-assignment/src/App.jsx
+++ imagesat-technical-assignment/src/App.jsx
@@ -2,9 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  // let checkedLocalStorage={}
   const [data, setData] = useState("Loading...");
-  // const [checkedUser, setCheckedUser] = useState(getLocalCheckboxStatus());
   useEffect(() => {
     const url = "https://run.mocky.io/v3/e521013e-bfb8-45aa-8406-a1d37cbbd717";
 
@@ -33,15 +31,12 @@ function App() {
             </li>
           </div>
         ));
-        // console.log(listItems);
-        // window.localStorage.clear();
-        // localStorage.setItem('data',json)
         setData(listItems);
       } catch (error) {
         console.log("error", error);
       }
     };
-    
+
     fetchData();
   }, []);
   return (
@@ -49,47 +44,26 @@ function App() {
       <ul>{data}</ul>
     </div>
   );
-  
-  
 
-  // function getLocalCheckboxStatus (){
-  //   const checkboxStat={}
-  //   for(let i=0;i<localStorage.length;i++){
-
-  //      checkboxStat[localStorage.key(i)] = window.localStorage.getItem(localStorage.key(i));
-  //   }
-  //   console.log(checkboxStat);
-  //   // console.log(localStorage.key(1));
-  // }
-  
   function setCheckbox(e, key) {
     console.log(e.target.checked);
     if (e.target.checked === true) {
-      // let updatedCheckedUser={...checkedUser}
-      // updatedCheckedUser[key]=true
-      // setCheckedUser(...checkedUser)
-      // localStorage.setItem(checkedLocalStorage[key], true);
       localStorage.setItem(key, true);
     } else {
-      // localStorage.setItem(checkedLocalStorage[key], false);
       localStorage.setItem(key, false);
     }
   }
-  
+
   function isChecked(key) {
     const checkboxStat = window.localStorage.getItem(key);
-    // console.log(e);
-    if (checkboxStat === 'true') {
-      // console.log(checkboxStat);
-      console.log(Boolean(checkboxStat),key);
+
+    if (checkboxStat === "true") {
+      console.log(Boolean(checkboxStat), key);
       return true;
-    } 
-    else {
-      // console.log(key, checkboxStat);
+    } else {
       return false;
     }
   }
 }
-
 
 export default App;
